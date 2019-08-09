29bfc6e9cc13a7c60ecfbb550be670a1a17da22b
paiandot <paiandot@gmail.com>
2019-08-09 16:13:46 +0900

    [feat] default setting

---
+++ test.js
@@ -0,0 +1,14 @@
+
+function func2(ins) {
+  let input = ins;
+  
+  
+  let result = input;
+  return result;
+}
+
+
+$( document ).ready(function() {
+    let result = func2("testtest");
+    console.log(result);
+});
